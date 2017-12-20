7a2aa9c98d204c201487f205af6b5d5dc40d2280
Laura Fleig <laurafleig@Lauras-MacBook-Pro.local>
2017-12-20 11:09:41 -0800

    saving changes

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -3,12 +3,15 @@ function addNewRow(){
 	var newRow = $('<tr>')
 	newRow.addClass('new-row')
 	$('.Chart-Content').append(newRow)
+	var thScope = $('<th scope = "col">')
+	newRow.append(thScope)
 
 }
-	
+
 
 
 $(document).ready(function () {
+
 
    var config = {
     apiKey: "AIzaSyB52UKC3YPbYotshARmKoVljIehyZBDjiI",
@@ -24,6 +27,8 @@ $(document).ready(function () {
 
   var database = firebase.database()
 
+
+
 	var trainName = ""
 	var destination = ""
 	var firstTrainTime = 0
@@ -35,10 +40,20 @@ $(document).ready(function () {
 		trainName = $('#trainName').val().trim()
 		destination = $('#destination').val().trim()
 		frequency = $('#frequency').val().trim()
-		firstTrainTime = $('#trainTime').val().trim()
+		firstTrainTime = $('#trainTime').val(moment(firstTrainTime).format("h:mm")).trim()
+		console.log(firstTrainTime)
+		
+
+		
+
+
+
+
 		minAway = firstTrainTime +  frequency
 
-		database.ref().set({
+
+
+		database.ref().push({
 			Name: trainName,
         	Destination: destination,
         	Frequency: frequency,
@@ -47,25 +62,55 @@ $(document).ready(function () {
 
         });
 
+     
+
 
 	});
 
-database.ref().on("value", function(snapshot) {
-      // Log everything that's coming out of snapshot
-      console.log(snapshot.val());
-      console.log(snapshot.val().Name);
-      console.log(snapshot.val().Destination);
-      console.log(snapshot.val().Frequency);
-      console.log(snapshot.val().FirstTrain);
-      console.log(snapshot.val().MinutesAway);
-      // Change the HTML to reflect
+database.ref().on("child_added", function(snapshot) {
+   
+	var newRow = $('<tr>')
+	newRow.addClass('new-row')
+	var thScope = $('<th>')
+	thScope.attr('scope', 'col')
+	thScope.text(snapshot.val().Name);
+
+	var thScope2 = $('<th>')
+	thScope2.attr('scope', 'col')
+	thScope2.text(snapshot.val().Destination)
+
+	var thScope3 = $('<th>')
+	thScope3.attr('scope', 'col')
+	thScope3.text(snapshot.val().Frequency)
+
+	var thScope4 = $('<th>')
+	thScope4.attr('scope', 'col')
+	thScope4.text(snapshot.val().FirstTrain)
+
+	var thScope5 = $('<th>')
+	thScope5.attr('scope', 'col')
+	thScope5.text(snapshot.val().MinutesAway)
 
 
-      $('.train-name').text(snapshot.val().Name);
-      $('.destination-name').text(snapshot.val().Destination);
-      $('.frequency').text(snapshot.val().Frequency);
-      $('.next-arrive').text(snapshot.val().FirstTrain)
-      $('.time-btwn').text(snapshot.val().MinutesAway)
+	newRow.append(thScope)
+	newRow.append(thScope2)
+	newRow.append(thScope3)
+	newRow.append(thScope4)
+	newRow.append(thScope5)
+
+
+	$('.Chart-Content').append(newRow)
+	
+	
+     
+
+
+
+      // $('.train-name').text(snapshot.val().Name);
+      // $('.destination-name').text(snapshot.val().Destination);
+      // $('.frequency').text(snapshot.val().Frequency);
+      // $('.next-arrive').text(snapshot.val().FirstTrain)
+      // $('.time-btwn').text(snapshot.val().MinutesAway)
     
 })
 	, function(errorObject) {
