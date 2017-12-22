87f4b103c21b2d94d45452883d5faa759ac50c4e
Laura Fleig <laurafleig@Lauras-MacBook-Pro.local>
2017-12-21 17:44:51 -0800

    working

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -24,6 +24,7 @@ $(document).ready(function () {
 	var firstTrainTime = 0
 	var frequency = 0
 	var tMinutesTillTrain = 0
+	var diffTime = 0
 
 	$('.submit').on('click', function(event){
 		event.preventDefault()
@@ -32,93 +33,88 @@ $(document).ready(function () {
 		frequency = $('#frequency').val().trim()
 		firstTrainTime = $('#trainTime').val().trim()
 
-		var firstTimeConverted = moment(firstTrainTime, "hh:mm a").subtract(1, "years")
-    	console.log(firstTimeConverted);
-
+		var firstTimeConverted = moment(firstTrainTime, "h:mm a")
     	var currentTime = moment();
-    	console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm a"));
+ 
+		//If the current time occurs before the first train time
+		if (firstTimeConverted > currentTime) {
 
-    	var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    	console.log("DIFFERENCE IN TIME: " + diffTime);
+    		var diffTime = moment(firstTimeConverted).diff(moment(), 'minutes')
 
-    	var tRemainder = diffTime % frequency;
-    	console.log(tRemainder);
+			database.ref().push({
+				Name: trainName,
+        		Destination: destination,
+        		Frequency: frequency,
+        		FirstTrain: firstTrainTime,
+        		MinutesAway: diffTime
 
-    	var tMinutesTillTrain = frequency - tRemainder;
-   		console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+        	})
 
-   		var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    	console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm, a"));
+		} else {
 
-		database.ref().push({
-			Name: trainName,
-        	Destination: destination,
-        	Frequency: frequency,
-        	FirstTrain: firstTrainTime,
-        	MinutesAway: tMinutesTillTrain,
+			var firstTimeConverted = moment(firstTrainTime, "hh:mm a").subtract(1, "years")
 
-        });
+    		var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
-     
+    		var tRemainder = diffTime % frequency;
 
+    		var tMinutesTillTrain = frequency - tRemainder;
 
-	});
+   			var nextTrain = moment().add(tMinutesTillTrain, "minutes");
 
-database.ref().on("child_added", function(snapshot) {
+			database.ref().push({
+				Name: trainName,
+        		Destination: destination,
+        		Frequency: frequency,
+        		FirstTrain: firstTrainTime,
+        		MinutesAway: tMinutesTillTrain,
+
+        	})
+
+		}
+
+    });
+
+	database.ref().on("child_added", function(snapshot) {
    
-	var newRow = $('<tr>')
-	newRow.addClass('new-row')
-	var thScope = $('<th>')
-	thScope.attr('scope', 'col')
-	thScope.text(snapshot.val().Name);
+		var newRow = $('<tr>')
+		newRow.addClass('new-row')
+		var thScope = $('<th>')
+		thScope.attr('scope', 'col')
+		thScope.text(snapshot.val().Name);
 
-	var thScope2 = $('<th>')
-	thScope2.attr('scope', 'col')
-	thScope2.text(snapshot.val().Destination)
+		var thScope2 = $('<th>')
+		thScope2.attr('scope', 'col')
+		thScope2.text(snapshot.val().Destination)
 
-	var thScope3 = $('<th>')
-	thScope3.attr('scope', 'col')
-	thScope3.text(snapshot.val().Frequency)
+		var thScope3 = $('<th>')
+		thScope3.attr('scope', 'col')
+		thScope3.text(snapshot.val().Frequency)
 
-	var thScope4 = $('<th>')
-	thScope4.attr('scope', 'col')
-	thScope4.text(snapshot.val().FirstTrain)
+		var thScope4 = $('<th>')
+		thScope4.attr('scope', 'col')
+		thScope4.text(snapshot.val().FirstTrain)
 
-	var thScope5 = $('<th>')
-	thScope5.attr('scope', 'col')
-	thScope5.text(snapshot.val().MinutesAway)
-
-
-	newRow.append(thScope)
-	newRow.append(thScope2)
-	newRow.append(thScope3)
-	newRow.append(thScope4)
-	newRow.append(thScope5)
+		var thScope5 = $('<th>')
+		thScope5.attr('scope', 'col')
+		thScope5.text(snapshot.val().MinutesAway)
 
 
-	$('.Chart-Content').append(newRow)
+		newRow.append(thScope)
+		newRow.append(thScope2)
+		newRow.append(thScope3)
+		newRow.append(thScope4)
+		newRow.append(thScope5)
+
+
+		$('.Chart-Content').append(newRow)
 	
-	
-     
-
-
-
-      // $('.train-name').text(snapshot.val().Name);
-      // $('.destination-name').text(snapshot.val().Destination);
-      // $('.frequency').text(snapshot.val().Frequency);
-      // $('.next-arrive').text(snapshot.val().FirstTrain)
-      // $('.time-btwn').text(snapshot.val().MinutesAway)
     
-})
-	, function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
-    }
-
-	 
-
-
-
+	})
 	
+
+
+
 
 
 
